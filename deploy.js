5a8c7a57932c325dcfdf270505b6aef6addf8b4a
Ethan Reece <66654040+Veixan@users.noreply.github.com>
2020-11-22 14:36:39 -0700

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.172.243.31',
+      host: '54.86.18.208',
       username: 'ubuntu',
       privateKey: 'todo-handson.ppk'
     })
